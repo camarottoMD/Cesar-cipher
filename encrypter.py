733ce84ff47dde1daa06ab94e89d325c0fb3d31e
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-08-05 12:24:06 -0300

    first commit. V1 da cifra de cesar. Idelização futura, fazer ele compreender caracteres especiais e espaços para senhas e frases chaves mais complexas

---
+++ encrypter.py
@@ -0,0 +1,52 @@
+#ord(caractere) + base - variancia
+
+class CesarCypher:
+
+    def __init__(self):
+        pass
+
+    def encrypter(self, texto: str, variance: int) -> str:
+        lista = []
+        for caractere in texto:
+            if caractere.islower():
+                response = self.__encriptar_cesar_lower(caractere, variance)
+
+            elif caractere.isupper():
+                response = self.__encriptar_cesar_upper(caractere, variance)
+
+            lista.append(response)
+        return "".join(lista)
+
+    def decrypter(self, texto: str, variance: int) -> str:
+        lista = []
+        for caractere in texto:
+            if caractere.islower():
+                response = self.__decriptar_cesar_lower(caractere, variance)
+
+            elif caractere.isupper():
+                response = self.__decriptar_cesar_upper(caractere, variance)
+
+            lista.append(response)
+        return "".join(lista)
+
+    def __encriptar_cesar_upper(self, caractere: str, variance: int) -> str:
+        carac_encrypted = chr((ord(caractere) - ord("A") + variance) % 26 + ord('A'))
+        return carac_encrypted
+
+    def __encriptar_cesar_lower(self, caractere: str, variance: int) -> str:
+        carac_encrypted = chr((ord(caractere) - ord("a") + variance) % 26 + ord('a'))
+        return carac_encrypted
+    
+    def __decriptar_cesar_lower(self, caractere: str, variance: int) -> str:
+        carac_decrypted = chr((ord(caractere) - ord('a') - variance) % 26 + ord('a'))
+        return carac_decrypted
+
+    def __decriptar_cesar_upper(self, caractere: str, variance: int) -> str:
+        carac_decrypted = chr((ord(caractere) - ord('A') - variance) % 26 + ord('A'))
+        return carac_decrypted
+    
+cripta = CesarCypher()
+encriptado = cripta.encrypter('Me ajuda yoda, me ajuda, mata o kazhix pelo menos', 3)
+print(encriptado)
+decriptado = cripta.decrypter(encriptado, 3) #tem que saber a variancia para poder acertar a casa certa
+print(decriptado)
